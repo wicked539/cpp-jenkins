b2a4a176fc145d2af340860740ee88c82e481ce4
Sven Mueller <mueller@synyx.de>
2014-08-11 16:21:24 +0200

    added gcovr

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo 'deb http://pkg.jenkins-ci.org/debian binary/' >> /etc/apt/sources.list
+echo 'deb http://us.archive.ubuntu.com/ubuntu trusty main universe' >> /etc/apt/sources.list
 
 apt-get update
-apt-get install build-essential git curl unzip cmake jenkins -y
+apt-get install build-essential git curl unzip cmake jenkins gcovr -y
